eeab7620c8788804401f215205d5a8dabd5a3f14
Maulana Firdaus <maulana.firdaus@advotics.com>
2020-06-11 15:00:42 +0700

    initial commit

---
+++ android/app/src/main/kotlin/com/kpr/syariah/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.kpr.syariah
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/constants.dart
@@ -0,0 +1,5 @@
+library constants;
+
+const String APP_NAME = 'KPR Syariah';
+const String PADDING = '   ';
+const String CURRENCY = 'Rp.';
